74edf5462d554fd5ea438fa082996a88cb9bf30f
aq0khan <118698973+aq0khan@users.noreply.github.com>
2023-02-06 14:24:46 +0100

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@ import create_table
 import create_view
 import visualize_view
 import load_data_into_db
-read_csv.read_csv_from_s3('contiamo-challenge', 'data-engineer-challenge/') #bucket and folder name
+read_csv.read_csv_from_s3('', '') #bucket and folder name
 create_table.ddl('customer_table') #passing table name
 load_data_into_db.store_data('customer_table') #passing table name
 create_view
